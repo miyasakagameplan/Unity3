77a8376ea1783de529d1b3ca4ac4b29f3fd55451
Toya Miyasaka <50751696+miyasakagameplan@users.noreply.github.com>
2019-05-18 04:51:32 +0900

    Add files via upload

---
+++ Mirrror.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+var mir : int = 0 ;
+
+
+function Start () {
+	
+}
+
+function Update () {
+
+ //if(Input.GetKeyDown("down")){
+ //bflg += 1;
+ //}
+ if(Input.GetMouseButtonDown(2)){
+ mir+= 1;
+
+ }
+  switch (mir){
+
+   case 0:
+  	   transform.position.y = -18.5;
+   break;
+
+   case 1:
+  	   transform.position.y = 0;
+   break;
+
+   case 2:
+  	   mir = 0;
+   break;
+
+   }
+
+}
---
+++ Player.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+	
+}
+
+function Update () {
+	if(Input.GetMouseButton(0)){
+   transform.position.z += 0.025;
+   }
+   if(Input.GetMouseButton(1)){
+   transform.position.z -= 0.025;
+  }
+
+  transform.position.x += 0.025;
+
+
+}
